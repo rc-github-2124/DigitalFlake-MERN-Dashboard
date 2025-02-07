2c550303dd9af566343fad454b92986abbfe95a4
rohit chavan <r.rohitchavan724@gmail.com>
2025-02-07 17:18:18 +0530

    removed files

--- server/src/models/Category.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const categorySchema = new mongoose.Schema({
-  categoryName: { type: String, required: true },
-  image: { type: String, required: true },
-  status: { type: Boolean, default: true },
-});
-
-module.exports = mongoose.model("Category", categorySchema);
